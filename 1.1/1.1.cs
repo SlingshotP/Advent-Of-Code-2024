02d90ce65951e6ba4b2b4735dd8e1a50064198a8
Samuel Heath <samheath1@outlook.com>
2024-12-01 06:51:23 +0000

    Add project files.

---
+++ 1.1/1.1.cs
@@ -0,0 +1,39 @@
+﻿namespace Advent_of_Code_2024._1._1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("Text.txt");
+        //string[] lines = {"3   4", "4   3", "2   5", "1   3", "3   9", "3   3"};
+        int[][] data = new int[2][];
+
+        for (int i = 0; i < 2; i++)
+        {
+            data[i] = new int[lines.Length];
+            for (int j = 0; j < lines.Length; j++)
+            {
+                string[] line = lines[j].Split("   ");
+                data[i][j] = int.Parse(line[i]);
+            }
+                
+        }
+
+        Array.Sort(data[0]);
+        Array.Sort(data[1]);
+        int count = 0;
+        for (int i = 0;i < data[0].Length; i++) //data[0].Length == data[1].Length
+        {
+            //Console.WriteLine($"Difference: {data[1][i] - data[0][i]}");
+            if (data[1][i] > data[0][i])
+            {
+                count += data[1][i] - data[0][i];
+            }
+            else count += data[0][i] - data[1][i];
+
+        }
+        Console.WriteLine(count);
+
+        Console.ReadKey();
+    }
+}
---
+++ 1.2/1.2.cs
@@ -0,0 +1,43 @@
+﻿namespace Advent_of_Code_2024._1._2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("Text.txt");
+        //string[] lines = {"3   4", "4   3", "2   5", "1   3", "3   9", "3   3"};
+        int[][] data = new int[2][];
+
+        for (int i = 0; i < 2; i++)
+        {
+            data[i] = new int[lines.Length];
+            for (int j = 0; j < lines.Length; j++)
+            {
+                string[] line = lines[j].Split("   ");
+                data[i][j] = int.Parse(line[i]);
+            }
+
+        }
+
+        int mainCount = 0;
+        int subCount = 0;
+        for (int i = 0; i < data[0].Length; i++) //data[0].Length == data[1].Length
+        {
+            for (int j = 0; j < data[1].Length; j++)
+            {
+                if (data[0][i] == data[1][j])
+                {
+                    //Console.Write("Matching data: ");
+                    subCount++;
+                    //Console.Write(subCount);
+                    //Console.WriteLine();
+                }
+            }
+            mainCount += data[0][i] * subCount;
+            subCount = 0;
+        }
+        Console.WriteLine(mainCount);
+
+        Console.ReadKey();
+    }
+}
