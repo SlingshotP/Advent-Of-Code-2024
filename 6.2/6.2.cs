7618971b0e2199350081f5bcd1dc6d587408c114
Samuel Heath <samheath1@outlook.com>
2024-12-06 18:49:43 +0000

    day 6 lol i suck huh what who said thatr

--- 6.2/6.2.cs
+++ 6.2/6.2.cs
@@ -1,134 +1,130 @@
-﻿namespace _6._2
+﻿namespace _6._2;
+internal class Program
 {
-    internal class Program
+    // 1599 tried, incorrect
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string[] lines = File.ReadAllLines("Text.txt");
+        char[][] grid = new char[lines.Length][];
+
+        int x = -1, y = -1;
+        char d = '^';
+        for (int i = 0; i < lines.Length; i++)
         {
-            string[] lines = File.ReadAllLines("Text.txt");
-            char[][] grid = new char[lines.Length][];
-
-            for (int i = 0; i < lines.Length; i++)
+            string line = lines[i];
+            grid[i] = new char[line.Length];
+            for (int j = 0; j < line.Length; j++)
             {
-                string line = lines[i];
-                grid[i] = new char[line.Length];
-                for (int j = 0; j < line.Length; j++)
+                grid[i][j] = line[j];
+                if (grid[i][j] == d)
                 {
-                    grid[i][j] = line[j];
+                    x = j;
+                    y = i;
                 }
             }
-
-            int count = 0;
-            for (int i = 0; i < grid.Length; i++)
-            {
-                for (int j = 0; j < grid[i].Length; j++)
-                {
-                    if (grid[i][j] == '^') continue;
-                    var newGrid = grid.Select(r => r.ToArray()).ToArray();
-                    newGrid[i][j] = '#';
-
-                    if (Loop(newGrid))
-                    {
-                        count++;
-                        //Console.WriteLine($"Looped at row {i}, column {j}");
-                    }
-                    //Console.WriteLine("One position");
-                }
-
-            //Console.WriteLine("One row");
-            }
-            Console.WriteLine(count);
-
-            Console.ReadKey();
         }
-        static bool Loop(char[][] grid)
+        //Console.WriteLine($"Guard is at row {y}, column {x}");
+
+        List<(int x, int y, char d)> directionalHistory = [];
+        Loop(grid, x, y, d, directionalHistory);
+
+        var history = directionalHistory.Select(i => (i.x, i.y)).Distinct().ToList();
+
+        //Console.SetCursorPosition(0, 0);
+        //for (int i = 0; i < grid.Length; i++)
+        //{
+        //    for (int j = 0; j < grid[i].Length; j++)
+        //    {
+        //        Console.Write(grid[i][j]);
+        //    }
+        //    Console.WriteLine();
+        //}
+
+        int count = 0;
+        foreach (var (j, i) in history)
         {
-            char[] player = ['^', '<', '>', 'v'];
+            //Console.SetCursorPosition(j, i);
+            //Console.Write('X');
 
-            int count = 0;
-            bool @out = false;
-            while (!@out)
+            var temp = grid[i][j];
+            grid[i][j] = '#';
+
+            if (Loop(grid, x, y, d, []))
             {
-                for (int i = 0; i < grid.Length && !@out; i++)
-                {
-                    for (int j = 0; j < grid[i].Length; j++)
-                    {
-                        if (!player.Contains(grid[i][j]))
-                        {
-                            count++; continue;
-                        }
+                //Console.WriteLine($"({j}, {i})");
+                count++;
+            }
+            grid[i][j] = temp;
+            //Console.WriteLine("One pos");
+        }
+        //Console.SetCursorPosition(0, grid.Length);
+        Console.WriteLine(count);
+    }
+    static bool Loop(char[][] grid, int x, int y, char d, List<(int x, int y, char d)> history)
+    {
+        while (true)
+        {
+            if (history.Contains((x, y, d)))
+            {
+                //Console.WriteLine($"{x}, {y}, {d}");
+                return true;
+            }
 
-                        if (i == 0 && grid[i][j] == '^'
-                            || j == 0 && grid[i][j] == '<'
-                            || i == grid.Length - 1 && grid[i][j] == 'v'
-                            || j == grid[i].Length - 1 && grid[i][j] == '>')
-                        {
-                            grid[i][j] = 'X';
-                            @out = true;
-                            return false;
-                        }
-                        // Up
-                        if (grid[i][j] == '^')
-                        {
-                            grid[i][j] = 'X';
-                            if (grid[i - 1][j] == '#')
-                            {
-                                grid[i][j + 1] = '>';
-                            }
-                            else
-                            {
-                                grid[i - 1][j] = '^';
-                            }
-                        }
-                        // Right
-                        else if (grid[i][j] == '>')
-                        {
-                            grid[i][j] = 'X';
-                            if (grid[i][j + 1] == '#')
-                            {
-                                grid[i + 1][j] = 'v';
-                            }
-                            else
-                            {
-                                grid[i][j + 1] = '>';
-                            }
-                        }
-                        // Down
-                        else if (grid[i][j] == 'v')
-                        {
-                            grid[i][j] = 'X';
-                            if (grid[i + 1][j] == '#')
-                            {
-                                grid[i][j - 1] = '<';
-                            }
-                            else
-                            {
-                                grid[i + 1][j] = 'v';
-                            }
-                        }
-                        // Left
-                        else if (grid[i][j] == '<')
-                        {
-                            grid[i][j] = 'X';
-                            if (grid[i][j - 1] == '#')
-                            {
-                                grid[i - 1][j] = '^';
-                            }
-                            else
-                            {
-                                grid[i][j - 1] = '<';
-                            }
-                        }
-                        count++;
-                        if (count > 44162585)
-                        {
-                            //Console.WriteLine("Looped");
-                            return true;
-                        }
-                    }
+            history.Add((x, y, d));
+            if (y == 0 || x == 0 || y == grid.Length - 1 || x == grid[y].Length - 1)
+            {
+                return false;
+            }
+
+            // Up
+            if (d == '^')
+            {
+                if (grid[y - 1][x] == '#')
+                {
+                    d = '>';
+                }
+                else
+                {
+                    y--;
                 }
             }
-            Console.WriteLine("Looped!");
-            return true;
+            // Right
+            else if (d == '>')
+            {
+                if (grid[y][x + 1] == '#')
+                {
+                    d = 'v';
+                }
+                else
+                {
+                    x++;
+                }
+            }
+            // Down
+            else if (d == 'v')
+            {
+                if (grid[y + 1][x] == '#')
+                {
+                    d = '<';
+                }
+                else
+                {
+                    y++;
+                }
+            }
+            // Left
+            else //if (d == '<')
+            {
+                if (grid[y][x - 1] == '#')
+                {
+                    d = '^';
+                }
+                else
+                {
+                    x--;
+                }
+            }
         }
     }
 }
